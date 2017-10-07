6825838e9429c811c29aa1f85da322676a3cc4ce
Lucas Medeiros <lucastoc@gmail.com>
2017-10-07 15:41:46 -0300

    use BASE_LOGIN_URI inside the login method only

--- lib/zype_sdk/client.rb
+++ lib/zype_sdk/client.rb
@@ -4,6 +4,7 @@ require 'forwardable'
 require 'httparty'
 
 module ZypeSDK
+  # Public: HTTP wrapper responsible to communicate with the Zype WEB API
   class Client
     extend Forwardable
 
@@ -14,9 +15,11 @@ module ZypeSDK
     end
 
     def login(username:, password:)
-      credentials = "username=#{username}&password=#{password}"
-      params = "grant_type=password&client_id=#{client_id}&client_secret#{client_secret}&#{credentials}"
-      zype_response = post("oauth/token?#{params}")
+      user_credentials = "username=#{username}&password=#{password}"
+      client_credentials = "client_id=#{client_id}&client_secret#{client_secret}"
+      params = "grant_type=password&#{client_credentials}&#{user_credentials}"
+
+      zype_response = HTTParty.post("#{BASE_LOGIN_URI}/oauth/token?#{params}")
       Response.new(zype_response.code, zype_response.parsed_response)
     end
 
@@ -32,10 +35,6 @@ module ZypeSDK
     BASE_LOGIN_URI = 'https://login.zype.com'
 
     attr_reader :config
-
-    def post(uri, params = {})
-      HTTParty.post("#{BASE_LOGIN_URI}/#{uri}", params)
-    end
 
     def_delegators :@config, :app_key, :client_id, :client_secret
   end
